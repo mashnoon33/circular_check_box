5f28a278d5e21fcfbd66d2ca78a25a367a07322a
Lorenzo <djonline96@gmail.com>
2021-02-23 10:58:10 +0100

    Add a normal checkbox in example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,25 +6,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Circular Checkbox Example'),
@@ -35,15 +22,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -51,28 +29,52 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-bool selected=true ;
-
+  bool selected = true;
 
   @override
   Widget build(BuildContext context) {
- 
     return Scaffold(
       appBar: AppBar(
-       
         title: Text(widget.title),
       ),
       body: Center(
         child: Column(
-        
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-             ListTile(
-                leading: CircularCheckBox(value: this.selected, checkColor: Colors.white  ,activeColor: Colors.green, inactiveColor: Colors.redAccent, disabledColor: Colors.grey , onChanged: (val) => this.setState(() { this.selected= !this.selected ;}) ),
-                title: Text("Click me"),
-                onTap: ()=> this.setState(() { this.selected= !this.selected ;}),
+            ListTile(
+              leading: Checkbox(
+                value: this.selected,
+                checkColor: Colors.white,
+                activeColor: Colors.green,
+                onChanged: (val) => this.setState(
+                  () {
+                    this.selected = !this.selected;
+                  },
+                ),
               ),
-            
+              title: Text("Click me"),
+              onTap: () => setState(() => this.selected = !this.selected),
+            ),
+            ListTile(
+              leading: CircularCheckBox(
+                value: this.selected,
+                checkColor: Colors.white,
+                activeColor: Colors.green,
+                inactiveColor: Colors.redAccent,
+                disabledColor: Colors.grey,
+                onChanged: (val) => this.setState(
+                  () {
+                    this.selected = !this.selected;
+                  },
+                ),
+              ),
+              title: Text("Click me"),
+              onTap: () => this.setState(
+                () {
+                  this.selected = !this.selected;
+                },
+              ),
+            ),
           ],
         ),
       ),
